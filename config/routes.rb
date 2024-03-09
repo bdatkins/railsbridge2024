83a85659a0d767bd80d92a750d2369ad7a51e664
Barbara Atkins <bdatkins@gmail.com>
2024-03-09 02:58:47 +0000

    Pending changes exported from your codespace

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
